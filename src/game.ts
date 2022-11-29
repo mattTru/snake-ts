a5f80ff9b028be64ad564e8bb240ba235f443fa1
mattTru <49438976+mattTru@users.noreply.github.com>
2022-11-29 15:07:15 +0100

    Upgraded codes, added eatBox and game mode & added css

--- src/game.ts
+++ src/game.ts
@@ -3,55 +3,105 @@ import { Snake } from "./snake/snake";
 import { Direction } from "./snake/direction";
 import { Dimension } from "./grid/dimension";
 
-const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector('canvas');
-const context: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext('2d');
-
 export class Game {
     static grid: Grid;
-    static snake: { x: number; y: number; }[];
+    static snake: {x: number; y: number; }[];
     static direction: string;
     static speed: number;
+    static context: CanvasRenderingContext2D;
+    static caseX: number;
+    static caseY: number;
+    static eatBox: {x: number; y: number};
+    static isOver: boolean;
+    static isEat: boolean;
 
     public init() {
         Game.snake = new Snake().init();
         Game.direction = Direction.snake.right;
         Game.speed = 15;
+        Game.eatBox = {x: 0, y: 0};
+        Game.isOver = false;
+        Game.isEat = false;  
     }
 
     public start() {
         Game.grid = new Grid();
-        Game.grid.init(canvas, context);
+        Game.context = Game.grid.init();
 
         this.init();
+        this.generateEatBox();
         this.move();
     }
 
     public move() {
-        const snake = {...Game.snake[Game.snake.length - 1]};
-        const snakeAfterMove = snake;
+        const frontSnake = {...Game.snake[Game.snake.length - 1]};
         switch(Game.direction) {
             case Direction.snake.top:
-                snakeAfterMove.y = (snake.y === 0) ? (Dimension.grid.height - Dimension.grid.boxSize) : snake.y - Game.speed;
+                frontSnake.y = ({...Game.snake[Game.snake.length - 1]}.y === 0) ? (Dimension.grid.height - Dimension.grid.boxSize) : {...Game.snake[Game.snake.length - 1]}.y - Game.speed;
                 break;
             case Direction.snake.bottom:
-                snakeAfterMove.y = (snake.y + Dimension.grid.boxSize === Dimension.grid.height) ? 0 : snake.y + Game.speed;
+                frontSnake.y = ({...Game.snake[Game.snake.length - 1]}.y + Dimension.grid.boxSize === Dimension.grid.height) ? 0 : {...Game.snake[Game.snake.length - 1]}.y + Game.speed;
                 break;
             case Direction.snake.left:
-                snakeAfterMove.x = (snake.x === 0) ? (Dimension.grid.width - Dimension.grid.boxSize) : snake.x - Game.speed;
+                frontSnake.x = ({...Game.snake[Game.snake.length - 1]}.x === 0) ? (Dimension.grid.width - Dimension.grid.boxSize) : {...Game.snake[Game.snake.length - 1]}.x - Game.speed;
                 break;
             case Direction.snake.right:
-                snakeAfterMove.x = (snake.x + Dimension.grid.boxSize === Dimension.grid.width) ? 0 : snake.x + Game.speed;
+                frontSnake.x = ({...Game.snake[Game.snake.length - 1]}.x + Dimension.grid.boxSize === Dimension.grid.width) ? 0 : {...Game.snake[Game.snake.length - 1]}.x + Game.speed;
                 break;
         }
 
-        Game.snake.push(snakeAfterMove);
-        Game.snake.shift();
+        if (!Game.isEat) {
+            Game.snake.shift();
+        } else {
+            Game.isEat = !Game.isEat;
+        }
 
-        Game.grid.generate(context, Game.snake);
+        Game.snake.push(frontSnake);
+        Game.grid.generate(Game.context, Game.snake, Game.eatBox);
+
+        this.isDead();
+        this.eatBox();
 
         setTimeout(() => {
-            this.move();
+            if (!Game.isOver) {
+                window.requestAnimationFrame(this.move());
+            } else {
+                Game.grid.over(Game.context);
+            }
         }, 100);
+    }
+
+    public generateEatBox() {
+        Game.caseX = Math.floor(Math.random() * (Dimension.grid.width / 15)) * 15;
+        Game.caseY = Math.floor(Math.random() * (Dimension.grid.height / 15)) * 15;
+
+        Game.snake.forEach((box, index) => {
+            if (box.x === Game.caseX && box.y === Game.caseY) {
+                this.generateEatBox();
+                return;
+            }
+        })
+
+        Game.eatBox = {x: Game.caseX, y: Game.caseY};
+    }
+
+    public isDead() {
+        const frontSnake = {...Game.snake[Game.snake.length - 1]};
+        Game.snake.forEach((box, index) => {
+            if (Game.snake.length - 1 === index) {
+                return;
+            } else if ((frontSnake.x === box.x && frontSnake.y === box.y)) {
+                Game.isOver = true;
+            }
+        })
+    }
+
+    public eatBox() {
+        const frontSnake = {...Game.snake[Game.snake.length - 1]};
+        if (frontSnake.x === Game.eatBox.x && frontSnake.y === Game.eatBox.y) {
+            Game.isEat = true;
+            this.generateEatBox();
+        }
     }
 }
 
--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,19 +1,42 @@
 import { Dimension } from "./dimension";
 
 export class Grid {
-    public init(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+    public init() {
+        const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector('canvas');
+        const context: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext('2d');
+
         canvas.width = Dimension.grid.width;
         canvas.height = Dimension.grid.height;
         canvas.style.display = 'block';
+
+        return <CanvasRenderingContext2D>canvas.getContext('2d');
     }
 
-    public generate(context: CanvasRenderingContext2D, snake: { x: number; y: number; }[]) {
+    public generate(context: CanvasRenderingContext2D, snake: {x: number; y: number; }[], eatBox: {x: number; y: number;}) {
         context.clearRect(0, 0, Dimension.grid.width, Dimension.grid.height);
+
+        // eatBox
+        context.fillStyle = 'red'
+        context.beginPath();
+        context.rect(eatBox.x, eatBox.y, Dimension.grid.boxSize, Dimension.grid.boxSize);
+        context.fill();
+
+        // snake
         snake.forEach((item, index) => {
-            context.fillStyle = 'red';
+            context.fillStyle = '#34DE01';
             context.beginPath();
             context.rect(item.x, item.y, Dimension.grid.boxSize, Dimension.grid.boxSize);
             context.fill();
         })
+    }
+
+    public over(context: CanvasRenderingContext2D) {
+        context.fillStyle = 'white';
+        context.font = "25px Poppins";
+        context.fillText('Game over !', Dimension.grid.boxSize * 2, Dimension.grid.boxSize * 3);
+        const initButton = document.getElementById('init');
+        if (initButton !== null) {
+            initButton.style.display = 'flex';
+        }
     }
 } 
